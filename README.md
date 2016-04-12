1cb0847d8a83cad55f1c1b3f465c3c6cf88d44f0
fsantini <francesco.santini@gmail.com>
2016-04-12 20:01:27 +0200

    kept only images in tha pages branch

--- README.md
+++
@@ -1,4 +0,0 @@
-# rasPyCNCController
-CNC (Grbl) controller for a Raspberry Pi with 480x320 touchscreen
-
-The controller works with a joypad for the control of the machine.
--- rebuild_ui.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pyside-uic -o JogWidget/jogWidget_ui.py JogWidget/jogWidget.ui
-pyside-uic -o pyFileList/filelist_ui.py pyFileList/filelist.ui
-pyside-uic -o RunWidget/runWidget_ui.py RunWidget/runWidget.ui
-pyside-uic -o splash_ui.py splash.ui
-
